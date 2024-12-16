77cbdcf0806453726b296aa39467300d7509999f
Cj Linsangan <cjm.linsangan@gmail.com>
2024-12-16 21:31:49 +0800

    I added/changed something

--- system/dao/connect.php
+++ system/dao/connect.php
@@ -1,6 +1,6 @@
 <?php
   define('DB_SERVER', '127.0.0.1');
-  define('DB_USERNAME', 'pma');
+  define('DB_USERNAME', 'root');
   define('DB_PASSWORD', '');
   define('DB_PORT', '3307');
 	define('DB_NAME', 'tailtreats'); $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
