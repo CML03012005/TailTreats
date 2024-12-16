00bce2fe26df79b9543b1489c0b5fb2a1a7c759f
Cj Linsangan <cjm.linsangan@gmail.com>
2024-12-16 21:35:04 +0800

    I added/changed something

--- system/dao/connect.php
+++ system/dao/connect.php
@@ -2,8 +2,7 @@
   define('DB_SERVER', '127.0.0.1');
   define('DB_USERNAME', 'root');
   define('DB_PASSWORD', 'ILmiciaT12@1234');
-  define('DB_PORT', '3307');
-	define('DB_NAME', 'tailtreats'); $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+	define('DB_NAME', 'tailtreats'); $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
   if ($conn === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
   } else {
