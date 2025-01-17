fc3154ade7d564257983ddec719ce2645c0fc18a
Sei0217 <157269120+Sei0217@users.noreply.github.com>
2025-01-17 17:40:13 +0800

    Update edit.php

--- system/dao/edit.php
+++ system/dao/edit.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.14.5/dist/sweetalert2.all.min.js" integrity="sha256-1m4qVbsdcSU19tulVTbeQReg0BjZiW6yGffnlr/NJu4=" crossorigin="anonymous"></script>
+</head>
+<body>
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -19,17 +27,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 		if ($updateQuery == true) {
         
-            echo"<script>alert('You have updated your account successfully!');
-            window.location.href='../../index.php';
-            </script>";
+            echo"<script>
+            Swal.fire({
+                background: '#2D142C',
+                color: '#FFECD1',
+                icon: 'success',
+                title: 'Updated',
+                text: 'You have updated your account successfully!'
+            }).then(() => {
+                window.location.href = '../../index.php';
+            });
+          </script>";
+          exit;
             
         } else {
+            
             echo "<script>
-                alert('Failed to update account!')    
-                window.location.href = '../../forms/edituser.php';
+                Swal.fire({
+                    background: '#2D142C',
+                    color: '#FFECD1',
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Failed to update account!'
+                }).then(() => {
+                    window.location.href = './edituser.php';
+                });
             </script>";
+            exit;
         }
 		
 	}
 
 ?>
+
+</body>
+
+</html>
